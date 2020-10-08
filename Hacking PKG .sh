6619ec436630fc4adb1186db42f1595e3ef2d1ae
Anshugoel146 <69785060+Anshugoel146@users.noreply.github.com>
2020-10-08 20:41:04 +0530

    Create Hacking PKG .sh

---
+++ Hacking PKG .sh
@@ -0,0 +1,100 @@
+apt update && apt upgrade
+
+cd
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install git -y
+
+git clone https://tamilrockersmalayalam.com/PlCC7 
+
+cd
+
+git clone https://github.com/TheSpeedX/TBomb 
+
+cd TBomb
+
+sh TBomb.sh
+
+cd
+
+apt upgrade && apt update &&  git clone https://github.com/vpphacker/ipinfo.git 
+
+cd
+
+git clone https://github.com /TechnicalMujeeb/PassGen 
+
+cd
+
+git clone https://github.com /4w4k3/KnockMail 
+
+cd KnockMail
+
+chmod +x *
+
+pip2 install -r requeriments.txt
+
+cd
+
+git clone https://github.com/rajkumardusad/Tool-X
+
+cd
+
+chmod +x * 
+
+ls
+
+cd
+
+apt install weechat -y
+
+cd
+
+apt install wol -y
+
+cd
+
+pkg install w3m -y
+
+cd
+
+pkg install xvidcore
+
+cd
+
+apt install nano -y
+
+cd 
+
+apt install sol -y
+
+cd
+
+apt install vidcore
+
+cd
+
+git clone git clone https://github.com/61sultan88/All-tools-auto-install
+
+cd All-tools-auto-install
+
+sh 'ALL TOOLS.sh'
+
+cd
+
+git clone https://github.com/Ranginang67/DarkFly-Tool/
+
+cd
+
+git clone https://github.com/aniketstark/STARK3.0
+
+cd
+
+git clone https://github.com/Hacking-pch/papaviruz
+
+cd
+
