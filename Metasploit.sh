f2d6122b7798eef04f5fb843c5e169d375b60383
Anshugoel146 <69785060+Anshugoel146@users.noreply.github.com>
2020-10-08 20:45:39 +0530

    Create Metasploit.sh

---
+++ Metasploit.sh
@@ -0,0 +1,7 @@
+cd
+
+pkg update && pkg upgrade && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh
+
+cd
+
+ls
